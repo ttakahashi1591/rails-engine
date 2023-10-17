c2475cb6d285179ec707364ebaddb975a4a4083b
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-10-17 15:36:15 -0600

    feat: Add all models, assoiciaions, and validations as well as thier tests

---
+++ app/models/item.rb
@@ -0,0 +1,8 @@
+class Item < ApplicationRecord
+  belongs_to :merchant
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+
+  validates :name, presence: true
+  validates :unit_price, presence: true
+end
