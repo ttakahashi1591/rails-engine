4e297bc56ab8312d4d45235a058b990426e2be7c
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-10-17 15:53:32 -0600

    feat: Add Merchants request index and testing

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -7,5 +7,15 @@ describe "Merchants API" do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(Integer)
+
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
+    end
   end
 end
