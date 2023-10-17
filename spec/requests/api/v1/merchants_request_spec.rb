7a02b4ad8a773e035b61176fe7445439b519608d
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-10-17 14:25:56 -0600

    test: Add shoulda-matchers gem for validation testing

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -4,7 +4,7 @@ describe "Merchants API" do
   it "sends a list of merchants" do
     create_list(:merchant, 5)
 
-    get '/api/v1/books'
+    get '/api/v1/merchants'
 
     expect(response).to be_successful
   end
