e2bdad8672bec1e1fd6bc13129e660d58033276f
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-10-18 14:50:48 -0600

    test: Add spec test for Index Show request

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -30,4 +30,30 @@ describe "Items API" do
       expect(item[:attributes][:unit_price]).to be_a(Float)
     end
   end
+
+  it "can get one merchant by its id" do
+    id = create(:item).id
+    
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    
+    expect(items).to have_key(:data)
+    expect(items[:data]).to be_an(Array)
+    
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_an(String)
+
+    expect(item).to have_key(:attributes)
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_a(Float)
+  end
 end
