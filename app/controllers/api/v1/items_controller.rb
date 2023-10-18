e8a5d35c88a7b4a43ba486e0b56bae6967c7be84
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-10-18 10:45:55 -0600

    feat: Complete get a merchants items request, testing and passes through Postman

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    
+    merchant = Merchant.find(params[:merchant_id])
+    items = merchant.items
+    render json: ItemSerializer.new(items)
   end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -46,27 +46,33 @@ describe "Merchants API" do
   end
 
   it "sends a list of merchants items" do
-    id = create(:merchant).id
-    create_list(:item, 5)
+    merchant = create(:merchant)
+    create_list(:item, 5, merchant: merchant)
 
-    get "/api/v1/merchants/#{id}/items"
+    get "/api/v1/merchants/#{merchant.id}/items"
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+    items = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants[:data].count).to eq(5)
+    expect(items[:data].count).to eq(5)
 
-    merchants[:data].each do |merchant|
-      expect(merchants).to have_key(:data)
-      expect(merchants[:data]).to be_an(Array)
+    items[:data].each do |item|
+      expect(items).to have_key(:data)
+      expect(items[:data]).to be_an(Array)
       
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(String)
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(String)
 
-      expect(merchant).to have_key(:attributes)
-      expect(merchant[:attributes]).to have_key(:name)
-      expect(merchant[:attributes][:name]).to be_a(String)
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
     end
   end
 end
