8014b8166b5662ee9defb8f218eb9bb38a8f58ff
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-10-19 14:17:47 -0600

    Complete create response and updated MerchantItemController since the get merchant items postman test was breaking.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,4 +6,20 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      head 201
+      response.body = ItemSerializer.new(Item.last).to_json
+    else
+      head 401
+    end
+  end
+  
+  private
+  
+    def item_params
+      params.require(:item).permit(:name, :description, :unit_price, :merchant_id) 
+    end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -58,19 +58,23 @@ describe "Items API" do
   end
 
   it "can create a new item" do
+    merchant = create(:merchant)
+
     item_params = ({
                     name: 'Cheesecake',
                     description: 'Oreo Cookie',
-                    unit_price: 7.5
+                    unit_price: 7.5,
+                    merchant_id: merchant.id
                   })
     headers = {"CONTENT_TYPE" => "application/json"}
   
     post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    
     created_item = Item.last
   
     expect(response).to be_successful
-    expect(created_item.title).to eq(item_params[:name])
-    expect(created_item.author).to eq(item_params[:description])
-    expect(created_item.summary).to eq(item_params[:unit_price])
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
   end
 end
