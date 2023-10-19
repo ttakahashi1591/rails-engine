830b8d37597a1c3ea458889aa785556951905ec0
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-10-19 14:26:59 -0600

    Add destroy to Items resources in the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], :controller => 'merchant_items'
       end
-      resources :items, only: [:index, :show, :create]
+      resources :items, only: [:index, :show, :create, :destroy]
     end
   end
 end
